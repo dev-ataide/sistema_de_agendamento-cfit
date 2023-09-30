3162957383ca0c2dd3c377c71bd4080148c5f02a
Maycon <csa.devmaycon@gmail.com>
2023-09-30 14:05:57 -0300

    API de Cadastro

---
+++ src/database/db.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require("sequelize");
+
+const DB_NAME = 'cfit';
+const DB_USER = 'root'; // em alguns bancos o padrão user é root
+const DB_PASSWORD = 'maycon';
+
+const conexaodb = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false
+});
+
+module.exports = conexaodb;
